4f84d1042b273adeeb2aa5049004f8575602748f
Ivan Vasquez <ivanjv1234@gmail.com>
2022-01-30 15:54:23 -0600

    agregar about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def about
+  end
 end
 # rails generate controller pages esta es la manera de generar un controlador con todo sus complementos 
